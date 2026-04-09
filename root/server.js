0b13da63e0f9dde3047ecddf0af25f0d5c551e7c
martragon <martin.you@stonybrook.edu>
2026-04-09 10:35:13 -0400

    added / in wildcard

--- root/server.js
+++ root/server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'dist')));
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   });
 }
